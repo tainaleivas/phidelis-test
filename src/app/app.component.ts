ebfd2f63e5c27ffa28309e2c9f2d4a47e5eb299d
Tainá <leivastaina@gmail.com>
2024-08-22 01:48:02 -0300

    Feature: Add the first todo-list part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {NgFor} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    NgFor, 
+    FormsModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todo-list';
+
+  public task = "";
+  public items = ['item 1', 'item 2', 'item 3', 'item 4'];
+
+  addTask() {
+    this.items.push(this.task)
+  }
+
+  removeTask(item: string) {
+    this.items.splice(this.items.indexOf(item), 1)
+  }
+
 }
